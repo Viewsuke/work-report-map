1260d0794a49ad4a2287dd92c439525c969bbb06
Viewsuke <78889489+Viewsuke@users.noreply.github.com>
2026-02-08 01:25:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,8 @@ else:
         ).add_to(marker_cluster)
 
 # Use a unique key based on the length of filtered data to ensure refresh
-st_folium(m, width=1200, height=800, key=f"map_{len(filtered_df)}")
+st_folium(m, width=1200, height=800, key=f"map_{len(filtered_df)}",returned_objects=[])
+
 
 
 
